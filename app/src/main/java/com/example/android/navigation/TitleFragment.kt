fbe111bb510c7a8c627757a30ffaa59729f66069
LeeXiRouh <78258826+XiRouh@users.noreply.github.com>
2023-08-04 15:03:29 +0800

    Initial commit

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,15 +20,18 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TitleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-            R.layout.fragment_title, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,R.layout.fragment_title,container,false)
+
+        binding.playButton.setOnClickListener { view :View ->
+            view.findNavController()
+                .navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
+
 }
